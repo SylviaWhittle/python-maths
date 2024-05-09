c10632f87ce7bdbda4543834216f1bf26647a5e0
SylviaWhittle <sylviwhittle@gmail.com>
2024-05-09 14:13:35 +0100

    Add zero division exception and test

--- pythonmaths/arithmetic.py
+++ pythonmaths/arithmetic.py
@@ -52,7 +52,10 @@ def divide(x: int | float, y: int | float) -> float:
     >>> arithmetic.divide(5, 2)
         2.5
     """
-    return x / y
+    try:
+        return x / y
+    except ZeroDivisionError as e:
+        raise ZeroDivisionError("Cannot divide by zero, why would you even try that?") from e
 
 
 def multiply(x: int | float, y: int | float) -> float:
